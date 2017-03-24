60192395ab58b0f3fa4b8ac3336390755eff6386
Henrik Lievonen <henrik.lievonen@hotmail.com>
2017-03-24 22:54:36 +0200

    Fix file name case

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserDetailComponent } from './userdetail.component';
+import { UserDetailComponent } from './userDetail.component';
 import { RulesComponent, FilterRuleComponent, RewriterRuleComponent, MatcherRuleComponent } from './rules.component';
 
 @NgModule({
